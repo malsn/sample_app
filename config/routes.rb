9f9600059b9bb8560241bed9f9784e38848ef095
Sergey <malyshev@inter-forum.org>
2013-06-21 14:53:34 +0400

    Point in Sign-in-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   get "static_pages/home"
   
@@ -10,8 +15,6 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about'
   
   match '/contact', to: 'static_pages#contact'
-  
-  match '/signup', to: 'users#new'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
