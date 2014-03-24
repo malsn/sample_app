2448af0b9174782f661b033c55d7566756ebb803
Sergey <malyshev@inter-forum.org>
2014-03-24 19:14:23 +0400

    adding categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,24 @@
 class CategoriesController < ApplicationController
 
  def index
-	@categories = Category.all
+	@categories = Category.where(parent_id: nil)
  end
 
-
+ def create
+	@category = Category.new(params[:category])
+    if @category.save
+	  flash[:success] = "Category successfully created!"
+      redirect_to categories_path
+    else
+	  self.new
+      render 'new'
+    end
+ end
+ 
+ def new
+    @persons = User.all.map { |user| [user.name, user.id] }
+	@parcategories = Category.all.map { |cat| [cat.title, cat.id] }
+    @category = Category.new
+ end
+  
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   attr_accessible :parent_id, :text, :title, :user_id
-  belongs_to :user  
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :text, presence: true, length: { maximum: 500 }
+  has_many :subcategories, class_name: "Category", foreign_key: "parent_id"
+  belongs_to :parent, class_name: "Category"
+  belongs_to :user
 end
