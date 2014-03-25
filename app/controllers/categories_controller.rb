b78aff1f9e3d502310fa232b8b8cc3dcdbbe00d1
Sergey <malyshev@inter-forum.org>
2014-03-25 19:16:08 +0400

    adding categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,24 +1,36 @@
 class CategoriesController < ApplicationController
 
  def index
-	@categories = Category.where(parent_id: nil)
+	#@categories = Category.where(parent_id: nil).paginate(page: params[:page])
+	@categories = current_user.categories.paginate(page: params[:page])
+ end
+ 
+ def show
+	@category = Category.find(params[:id])
+	@subcategories = @category.subcategories
+	self.objlist
+	render 'edit'
  end
 
  def create
-	@category = Category.new(params[:category])
+	@category = current_user.categories.new(params[:category])
     if @category.save
 	  flash[:success] = "Category successfully created!"
       redirect_to categories_path
     else
-	  self.new
+	  self.objlist
       render 'new'
     end
  end
  
  def new
-    @persons = User.all.map { |user| [user.name, user.id] }
-	@parcategories = Category.all.map { |cat| [cat.title, cat.id] }
+    self.objlist
     @category = Category.new
+ end
+ 
+ def objlist
+	@persons = User.all.map { |user| [user.name, user.id] }
+	@parcategories = Category.all.map { |cat| [cat.title, cat.id] }
  end
   
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,7 @@
 class Category < ActiveRecord::Base
   attr_accessible :parent_id, :text, :title, :user_id
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :text, presence: true, length: { maximum: 500 }
   has_many :subcategories, class_name: "Category", foreign_key: "parent_id"
   belongs_to :parent, class_name: "Category"
   belongs_to :user
+  validates :title, :text, presence: true
 end
